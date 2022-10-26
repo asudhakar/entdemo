8cb674d6fec14b5a2a16288e010c621899a9f7d2
Sudhakar <asudhakar@live.in>
2022-10-26 21:56:47 +0530

    base code for create and retrive from a model

--- start/start.go
+++ start/start.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"entdemo/ent/user"
+	"fmt"
 	"log"
 
 	"entdemo/ent"
@@ -10,7 +12,7 @@ import (
 )
 
 func main() {
-	client, err := ent.Open("mysql", "<user>:<pass>@tcp(<host>:<port>)/<database>?parseTime=True")
+	client, err := ent.Open("mysql", "<username>:<password>@tcp(localhost:3306)/entdemo?parseTime=True")
 	if err != nil {
 		log.Fatalf("failed opening connection to mysql: %v", err)
 	}
@@ -19,4 +21,35 @@ func main() {
 	if err := client.Schema.Create(context.Background()); err != nil {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
+
+	//_, _ = CreateUser(context.Background(), client)
+	_, _ = QueryUser(context.Background(), client)
+
+}
+
+func CreateUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	u, err := client.User.
+		Create().
+		SetAge(30).
+		SetName("a8m").
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating user: %w", err)
+	}
+	log.Println("user was created: ", u)
+	return u, nil
+}
+
+func QueryUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	u, err := client.User.
+		Query().
+		Where(user.Name("a8m")).
+		// `Only` fails if no user found,
+		// or more than 1 user returned.
+		Only(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed querying user: %w", err)
+	}
+	log.Println("user returned: ", u)
+	return u, nil
 }
